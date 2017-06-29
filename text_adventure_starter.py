1b2c945e64b889352dd92269123da44ed61a5586
mchaavez <monichavez696@gmail.com>
2017-06-29 09:23:26 -0700

    Add files via upload

---
+++ text_adventure_starter.py
@@ -0,0 +1,20 @@
+start = '''
+You wake up one morning and find that you aren't in your bed; you aren't even in your room.
+You're in the middle of a giant maze.
+A sign is hanging from the ivy: "You have one hour. Don't touch the walls."
+There is a hallway to your right and to your left.
+'''
+
+
+print(start)
+
+
+# print("Type 'left' to go left or 'right' to go right.")
+user_input = input("Type 'left' to go left or 'right' to go right. ")
+if user_input == "left":
+    print("You decide to go left and run into a longer hallway.") # finished the story by writing what happens
+    print("decide to keep going left to the dark tunnel or right to where there is a scary town you have to pass by to get back to your room")
+
+elif user_input == "right":
+    print("You choose to go right and you run into a giant scare crow") # finished the story writing what happens
+    print()
