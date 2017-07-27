aeb591adc3f8cefd523bdb071b487cd448870736
mchaavez <monichavez696@gmail.com>
2017-07-27 09:24:39 -0700

    Create socialmedia.py

---
+++ socialmedia.py
@@ -0,0 +1,43 @@
+class User(object):
+    # Define the fields and methods for your object here.
+    userName= ""
+    userAge = ''
+    userID = ""
+    userConnections = ""
+
+    def __init__(self, name):
+        self.userName = name
+    def setUserAge(self, age):
+        self.userAge = age
+    def setUserID(self, id):
+        self.userID = ID
+    def setUserConnections(self, connections):
+        self.userConnections = connections
+
+    def getUserAge(self, age):
+        return self.userAge
+    def getUserID(self, id):
+        return self.userID
+    def getUserConnections(self, connections):
+        return self.userConnections
+
+def UserInfo(name, age, id, connections):
+
+class Network:
+    UserList = {
+
+
+    }
+
+
+def main():
+    # Define the program flow for your user interface here.
+
+# Runs your program.
+if __name__ == '__main__':
+    main():
+
+userName = input("What is your name: ")
+userAge = input("How old are u ?")
+userID = input(store_ID)
+userConnections = input("store_connections")
